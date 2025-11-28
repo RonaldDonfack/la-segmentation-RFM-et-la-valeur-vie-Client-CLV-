8c541f2b0210908fb9f6f9c7d1d3d1b77cafd57e
Brett273 <105299219+Brett273@users.noreply.github.com>
2025-11-28 14:56:25 +0100

    Update plan_action.py

--- myapp/pages/plan_action.py
+++ myapp/pages/plan_action.py
@@ -1,8 +1,3 @@
-"""
-MODULE PLAN D'ACTION - APPLICATION MARKETING
-Permet d'exporter les données actionnables et les visualisations pour passage à l'exécution
-"""
-
 import streamlit as st
 import pandas as pd
 import numpy as np
